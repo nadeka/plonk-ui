9876b85387c886d605a77c52adb7f6cf49930f40
Anni <nadeka@users.noreply.github.com>
2017-01-04 20:06:42 +0200

    Make HTTPS and WSS requests to backend in non-dev environments

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,9 +1,9 @@
 export const API_BASE_URL =
-  process.env.NODE_ENV === 'production' ?
-    'http://api.exoenzy.me' :
+  process.env.NODE_ENV !== 'development' ?
+    'https://api.exoenzy.me' :
     'http://localhost:6001';
 
 export const WS_BASE_URL =
-  process.env.NODE_ENV === 'production' ?
-    'ws://api.exoenzy.me' :
+  process.env.NODE_ENV !== 'development' ?
+    'wss://api.exoenzy.me' :
     'ws://localhost:6001';
