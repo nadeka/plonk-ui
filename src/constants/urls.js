80a3eccb56917fb6e0b5ee21f1a4611f4003321c
Anni <nadeka@users.noreply.github.com>
2017-01-04 15:39:12 +0200

    Change name of hosting bucket and api url

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,9 +1,9 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === 'production' ?
-    'http://plonk.eu-west-1.elasticbeanstalk.com' :
+    'http://api.exoenzy.me' :
     'http://localhost:6001';
 
 export const WS_BASE_URL =
   process.env.NODE_ENV === 'production' ?
-    'ws://plonk.eu-west-1.elasticbeanstalk.com' :
+    'ws://api.exoenzy.me' :
     'ws://localhost:6001';
